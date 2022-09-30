56367261c1b05c6cf1bd77cbb25a234da07160c0
xgqfrms <xgqfrms@outlook.com>
2022-10-01 01:50:27 +0800

    Create js6r.js

---
+++ demos/js6r.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2022-06-21
+ * @modified
+ *
+ * @description js
+ * @description js
+ * @difficulty Easy Medium Hard
+ * @time_complexity O(n)
+ * @space_complexity O(n)
+ * @augments
+ * @example
+ * @link https://leetcode.com/problems/???/
+ * @link https://leetcode-cn.com/problems/???/
+ * @solutions
+ *
+ * @best_solutions
+ *
+ */
+
+const log = console.log;
+
+// Generator
+const JS = (datas = [], debug = false) => {
+  let result = ``;
+  // do something...
+  return result;
+};
+
+
+export default JS;
+export {
+  JS,
+};
+
+/*
+
+
+// 测试用例 test cases
+const testCases = [
+  {
+    input: '4193 with words  ',
+    result: 4193,
+    desc: 'value equal to 4193',
+  },
+];
+for (const [i, testCase] of testCases.entries()) {
+  const result = JS(testCase.input);
+  log(`test case ${i} result: `, result === testCase.result ? `✅ passed` : `❌ failed`, result);
+  // log(`test case ${i} =`, testCase);
+}
+
+
+*/
