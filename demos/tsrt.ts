111befef3ec7e8b7a67b9c9532ff0bb99c79ae3a
xgqfrms <xgqfrms@outlook.com>
2022-10-01 01:50:59 +0800

    Create tsrt.ts

---
+++ demos/tsrt.ts
@@ -0,0 +1,62 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2022-08-01
+ * @modified
+ *
+ * @description
+ * @description
+ * @difficulty Easy Medium Hard
+ * @ime_complexity O(n)
+ * @space_complexity O(n)
+ * @augments
+ * @example
+ * @link https://leetcode.com/problems/ts/
+ * @link https://leetcode-cn.com/problems/ts/
+ * @solutions
+ *
+ * @best_solutions
+ *
+ */
+
+export {};
+
+const log = console.log;
+
+// Generator
+const TS = (datas = [], debug = false) => {
+  let result = ``;
+  // do something...
+  return result;
+};
+
+
+export default TS;
+export {
+  TS,
+};
+
+/*
+
+
+// 测试用例 test cases
+const testCases = [
+  {
+    input: '4193 with words  ',
+    result: 4193,
+    desc: 'value equal to 4193',
+  },
+];
+for (const [i, testCase] of testCases.entries()) {
+  const result = TS(testCase.input);
+  log(`test case ${i} result: `, result === testCase.result ? `✅ passed` : `❌ failed`, result);
+  // log(`test case i result: `, result === testCase.result ? `passed ✅` : `failed ❌`, result);
+  // log(`test case i =`, testCase);
+}
+
+
+*/
